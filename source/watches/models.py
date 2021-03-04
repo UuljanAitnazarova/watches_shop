9dcd4b9f7a67c9962514bfacf0e0c5da394a8c0b
uuljan <u.aitnazarova@gmail.com>
2021-03-04 20:02:32 +0600

    Created and made migrations to the Product model in whatches app

---
+++ source/watches/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Product(models.Model):
+
+    CATEGORY_CHOICE = [
+        ("men's watches", "мужские часы"),
+        ("ladies watches", "женские часы"),
+        ("watches for couples", "парные часы"),
+        ("other", "разное"),
+    ]
+
+
+
+    name = models.CharField(max_length=100, blank=False, null=False, verbose_name='Наименование товара')
+    description = models.TextField(max_length=2000, blank=True, null=True, verbose_name='Описание')
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICE, blank=False, null=False, default=CATEGORY_CHOICE[3], verbose_name='Категория')
+    product_availability = models.IntegerField(verbose_name='Остаток')
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Стоимость')
+
+
+    def __str__(self):
+        return self.name 
