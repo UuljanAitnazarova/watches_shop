66be516f694aef6e5fb5868dc71513545ab7307e
uuljan <u.aitnazarova@gmail.com>
2021-03-05 14:44:13 +0600

    Created product_detail_view, configured all the urls, in the main page only the products with availabilty more or equal to 1 will be listed

--- source/watches/views.py
+++ source/watches/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Product
+
+
+def product_list_view(request):
+    products = Product.objects.all()
+    return render(request, 'product_list.html', context={'products': products})
+
+
+def product_detail_view(request, pk):
+    product = get_object_or_404(Product, id=pk)
+    return render(request, 'product_detail.html', context={'product':product})
