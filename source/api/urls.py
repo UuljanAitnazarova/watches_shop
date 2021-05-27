6f54b11d468957cf05275c37b17f4c2e669e9fc2
uuljan <u.aitnazarova@gmail.com>
2021-05-27 18:48:46 +0600

    Implemented View, create and delete views

--- source/api/urls.py
+++ source/api/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path, include
 from rest_framework import routers
-from api.views import ProductViewSet, BucketViewSet, OrderApiView
+from api.views import ProductViewSet, CartView, OrderApiView, get_token_view
 
 
 router = routers.DefaultRouter()
 router.register(r'products', ProductViewSet)
-router.register(r'bucket', BucketViewSet)
 
 app_name = 'api'
 
 urlpatterns = [
     path('', include(router.urls)),
     path('orders/', OrderApiView.as_view()),
+    path('cart/', CartView.as_view()),
+    path('token/', get_token_view),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- source/api/views.py
+++ source/api/views.py
@@ -1,12 +1,16 @@
+import json
+
+from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
 from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import ensure_csrf_cookie
 from rest_framework import viewsets
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import generics
 
 from watches.models import Product, Order, ProductOrder
-from api.serializers import ProductSerializer, OrderSerializer, BucketSerializer
-
+from api.serializers import ProductSerializer, OrderSerializer
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
@@ -18,9 +22,62 @@ class OrderApiView(generics.ListCreateAPIView):
     serializer_class = OrderSerializer
 
 
-class BucketViewSet(viewsets.ModelViewSet):
-    queryset = ProductOrder.objects.all()
-    serializer_class = BucketSerializer
+class CartView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        cart = request.session.get('cart', {})
+        if cart:
+            data = json.dumps(cart)
+            print(data)
+            return HttpResponse(data)
+        response = JsonResponse({'error': 'no data in cart!'})
+        response.status_code = 400
+        return response
+
+
+    def post(self, request, *args, **kwargs):
+        cart = request.session.get('cart', {})
+        if request.body:
+            data = json.loads(request.body)
+            product = get_object_or_404(Product, id=data['product_id'])
+            if product.product_availability >= 1:
+                if not cart.get(str(data['product_id'])):
+                    cart[str(data['product_id'])] = 1
+                else:
+                    cart[str(data['product_id'])] += 1
+            else:
+                return JsonResponse({'error': 'No available products'})
+            request.session['cart'] = cart
+            return JsonResponse(cart)
+        response = JsonResponse({'error': 'No data provided!'})
+        response.status_code = 400
+        return response
+
+    def delete(self, request, *args, **kwargs):
+        cart = request.session.get('cart', {})
+        if request.body:
+            data = json.loads(request.body)
+            if cart.get(str(data['product_id'])):
+                balance = cart[str(data['product_id'])] - 1
+                if balance <= 0:
+                    del cart[str(data['product_id'])]
+                else:
+                    cart[str(data['product_id'])] = balance
+                request.session['cart'] = cart
+                return JsonResponse({'Deleted': 'Deleted'})
+            return JsonResponse({'error': 'No such product'})
+        return JsonResponse({'error': 'No data provided!'})
+
+
+
+
+
+
+@ensure_csrf_cookie
+def get_token_view(request, *args, **kwargs):
+    if request.method == 'GET':
+        return HttpResponse()
+    return HttpResponseNotAllowed('Only GET request are allowed')
 
 
 
